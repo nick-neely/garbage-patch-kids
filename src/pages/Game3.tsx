209f8e8ce20be9f46bcc584cbdf256a7779c684b
Jaiso <Jaisonanderson04@gmail.com>
2023-11-09 14:09:49 -0600

    Adding tube class

--- src/pages/Game3.tsx
+++ src/pages/Game3.tsx
@@ -1,18 +1,31 @@
 import  { CanvasView } from "../components/CanvasView";
+import PlayButton from "../components/PlayButton";
+import {Tube} from './sprites/Tube'
 
 
 const Game3 = () => {
     return (
         <body>
+            <div id="score"></div>
             <h1>Welcome to Flappy Bird!!</h1>
+            <PlayButton  />
             <canvas id = "playField"></canvas>
         </body>
     )
 };
 
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView){
+    view.drawInfo
+    gameOver = false;
+}
+
 
 function gameLoop (
-    view: CanvasView
+    view: CanvasView,
+    tubes: Tube[],
 ){}
 
 function startGame(view: CanvasView) {}
---
+++ src/sprites/Tube.tsx
@@ -0,0 +1,43 @@
+import {Vector} from '../types';
+
+export class Tube {
+
+    private tubeImage: HTMLImageElement = new Image();
+
+    constructor(
+        private tubeWidth: number,
+        private tubeHeight: number,
+        private position: Vector,
+        private tubeEnergy: number,
+        image: string
+    ){
+            this.tubeWidth = tubeWidth;
+            this.tubeHeight = tubeHeight;
+            this.position = position;
+            this.tubeEnergy = tubeEnergy;
+            this.tubeImage.src = image;
+     }   
+     get width(): number{
+        return this.tubeWidth;
+     }
+
+     get height(): number{
+        return this.tubeHeight;
+     }
+
+     get tubePosition(): Vector{
+        return this.position;
+     }
+
+     get energy(): number{
+        return this.tubeEnergy;
+     }
+
+     set energy(energy:number){
+         this.tubeEnergy = energy
+     }
+
+     get img(): number{
+        return this.tubeWidth;
+     }
+}
