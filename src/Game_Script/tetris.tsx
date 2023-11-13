725e234d066af5896f72fe32d396e7d140afa2c6
Dakota <dakotagarrison.14@gmail.com>
2023-11-13 10:57:49 -0600

    button function for game 2 added, calls tetris tsx

---
+++ src/Game_Script/tetris.tsx
@@ -0,0 +1,13 @@
+export class Tetris{
+    public canvas: HTMLCanvasElement;
+    public ctx;
+    
+    constructor(canvas: HTMLCanvasElement){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");  
+    }
+    GameRun(){
+
+    }
+    }
+    
--- src/pages/Game2.tsx
+++ src/pages/Game2.tsx
@@ -1,26 +1,28 @@
 import PlayButton from "../components/PlayButton";
-import tetrisCss from "../styles/tetris.module.css"
- 
-
+import tetrisCss from "../styles/tetris.module.css";
+import {Tetris} from "../Game_Script/tetris"
+const runGame = () => {
+    return (event: React.MouseEvent) => {
+        event.preventDefault();
+        const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+            let t = new Tetris(canvas)
+            t.GameRun()
+       }
+}
 const Game2 = () => {
     return (
-        <body>
+        <div>
         <div id={tetrisCss["canvas-cont"]}>
             <h1 className={tetrisCss["game_title"]}>Tetris</h1>
             <canvas id={tetrisCss["game_2_canvas"]}></canvas>
         </div>
-        <ul>
-            <li className={tetrisCss["game-menu"]}><PlayButton/></li>
-            <li className="game-menu"><button>Full Screen</button></li>
-        </ul>
-
+        <div onClick={runGame()}><PlayButton/></div>
         <div className={tetrisCss["rulesDropdown"]}>
             <button id={tetrisCss["rules"]}>↓ Show Instructions ↓</button>
             <div className ={tetrisCss["rulesContent"]}>
-        
             </div>
         </div>
-        </body>
+        </div>
     );
   };
 
