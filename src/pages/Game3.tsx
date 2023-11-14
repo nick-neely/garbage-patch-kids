18445603ac7adcd6ab007bfb75119ef42df3e9e0
Nick Neely <neelynickolas@gmail.com>
2023-11-14 08:48:12 -0600

    Fixed bugs

--- src/pages/Game3.tsx
+++ src/pages/Game3.tsx
@@ -2,27 +2,24 @@ import FlappyCss from "../styles/flappybird.module.css";
 import PlayButton from "../components/PlayButton";
 import React from "react";
 
-const Game3 = ()  => {
-    return (
-    
-        <body>
-            <div id={FlappyCss["flappy-cont"]}>
-                <div id={FlappyCss["arcade-screen"]}>
-                <canvas id={FlappyCss["canvas"]}>
-                </canvas>
-                    <div id={FlappyCss['game']}></div>
-                    <div id= {FlappyCss['block']}>Block</div>
-                    <div id = {FlappyCss["hole"]}></div>
-                    <div id ={FlappyCss["charcter"]}></div>
-                
-                <PlayButton/>
-                </div>
-            </div>
-            <script src="flappy_game.js"></script>    
-        </body>
-    )
-};
+const Game3 = () => {
+  return (
+    <div>
+      <div id={FlappyCss["flappy-cont"]}>
+        <div id={FlappyCss["arcade-screen"]}>
+          <canvas id={FlappyCss["flappy-canvas"]}></canvas>
+          <div id={FlappyCss["game"]}></div>
+          <div id={FlappyCss["block"]}>Block</div>
+          <div id={FlappyCss["hole"]}></div>
+          <div id={FlappyCss["charcter"]}></div>
 
+          <PlayButton />
+        </div>
+      </div>
+      <script src="flappy_game.js"></script>
+    </div>
+  );
+};
 
 export default Game3;
 
